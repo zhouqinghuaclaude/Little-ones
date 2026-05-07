87851b856ffae7c277dc27982037af9a5da8601c
zhouqinghuaclaude <qhzhou@icloud.com>
2026-05-07 16:16:46 +1000

    Update server.js

--- server.js
+++ server.js
@@ -955,6 +955,7 @@ db.query("ALTER TABLE kids ADD COLUMN IF NOT EXISTS last_birthday_celebrated INT
 db.query("ALTER TABLE kids ADD COLUMN IF NOT EXISTS avatar_prompt_sent BOOLEAN DEFAULT false").catch(() => {});
 db.query("ALTER TABLE kids ADD COLUMN IF NOT EXISTS gifts_received INTEGER DEFAULT 0").catch(() => {});
 db.query("ALTER TABLE kids ADD COLUMN IF NOT EXISTS parent_interests TEXT").catch(() => {});
+db.query("ALTER TABLE kids ADD COLUMN IF NOT EXISTS avatar_prompt_date TIMESTAMP DEFAULT NULL").catch(() => {});
 
 db.query(`CREATE TABLE IF NOT EXISTS activities (
  id SERIAL PRIMARY KEY,
